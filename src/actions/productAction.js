3b646d1abf32ecbb1bc1c19b05d3853bd7ce0837
anishgehlot <anish.gehlot@nagarro.com>
2024-01-09 21:33:18 +0530

    server added

--- src/actions/productAction.js
+++ src/actions/productAction.js
@@ -29,7 +29,15 @@ import {  ALL_PRODUCT_FAIL,
     DELETE_REVIEW_FAIL,
     CLEAR_ERRORS,
  } from "../constants/productConstants"
+
+ const config = {
+  headers: {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS"
+  }
+};
  export const getProduct =
+
  (keyword = "", currentPage = 1, price = [0, 25000], category, ratings = 0) =>
  async (dispatch) => {
    try {
@@ -41,7 +49,7 @@ import {  ALL_PRODUCT_FAIL,
        link = `${base_url}/api/v1/products?keyword=${keyword}&page=${currentPage}&price[gte]=${price[0]}&price[lte]=${price[1]}&category=${category}&ratings[gte]=${ratings}`;
      }
 
-     const { data } = await axios.get(link);
+     const { data } = await axios.get(link , config) ;
 
      dispatch({
        type: ALL_PRODUCT_SUCCESS,
@@ -67,7 +75,7 @@ export const getProductDetails = (id) => async (dispatch) => {
     try {
       dispatch({ type: PRODUCT_DETAILS_REQUEST });
   
-      const { data } = await axios.get(`${base_url}/api/v1/product/${id}`);
+      const { data } = await axios.get(`${base_url}/api/v1/product/${id}` , config);
   
       dispatch({
         type: PRODUCT_DETAILS_SUCCESS,
